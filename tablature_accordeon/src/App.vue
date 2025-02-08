069ea5b9cd5316e7d0ef2294017df0e294210e1a
Sylvain Anselmino <sylvain.anselmino@eur-ohm.com>
2025-02-08 23:53:50 +0100

    description textuelle des accords

--- tablature_accordeon/src/App.vue
+++ tablature_accordeon/src/App.vue
@@ -15,6 +15,25 @@ let currentNotes = ref([]);
       <div class="separatorV"></div>
       <LetterNotes @select-accord="currentNotes=$event" />
     </div>
+    
+  <div style="width: 400px">
+    <h1>Accords Majeurs</h1>
+    <div>Do majeur (C) : Do, Mi, Sol</div>
+    <div>Ré majeur (D) : Ré, Fa#, La</div>
+    <div>Mi majeur (E) : Mi, Sol#, Si</div>
+    <div>Fa majeur (F) : Fa, La, Do</div>
+    <div>Sol majeur (G) : Sol, Si, Ré</div>
+    <div>La majeur (A) : La, Do#, Mi</div>
+    <div>Si majeur (B) : Si, Ré#, Fa#</div>
+    <h1>Accords Mineurs</h1>
+    <div>Do mineur (Cm) : Do, Mib, Sol</div>
+    <div>Ré mineur (Dm) : Ré, Fa, La</div>
+    <div>Mi mineur (Em) : Mi, Sol, Si</div>
+    <div>Fa mineur (Fm) : Fa, Lab, Do</div>
+    <div>Sol mineur (Gm) : Sol, Sib, Ré</div>
+    <div>La mineur (Am) : La, Do, Mi</div>
+    <div>Si mineur (Bm) : Si, Ré, Fa#</div>
+  </div>
   </main>
 </template>
 
