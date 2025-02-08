ced767acf992d22a9ff4883898b11bff6cd1e7dd
Sylvain Anselmino <sylvain.anselmino@eur-ohm.com>
2025-02-09 00:01:07 +0100

    refactor file name

--- tablature_accordeon/src/App.vue
+++ tablature_accordeon/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from "vue";
-import LetterNotes from "./components/LetterNotes.vue";
+import LetterNotes from "./components/AccordLetters.vue";
 import Clavier from "./components/Clavier.vue";
 
 let currentNotes = ref([]);
@@ -10,30 +10,32 @@ let currentNotes = ref([]);
   <header></header>
 
   <main>
+    <div style="padding-left: 435px;"><button @click="currentNotes = []" style="width: 150px; height: 60px; background-color: lightgray; border-radius: 10px;cursor: pointer;" >Effacer</button></div>
     <div class="container">
       <Clavier :current-notes="currentNotes" />
       <div class="separatorV"></div>
-      <LetterNotes @select-accord="currentNotes=$event" />
+
+      <LetterNotes @select-accord="currentNotes = $event" />
     </div>
-    
-  <div style="width: 400px">
-    <h1>Accords Majeurs</h1>
-    <div>Do majeur (C) : Do, Mi, Sol</div>
-    <div>Ré majeur (D) : Ré, Fa#, La</div>
-    <div>Mi majeur (E) : Mi, Sol#, Si</div>
-    <div>Fa majeur (F) : Fa, La, Do</div>
-    <div>Sol majeur (G) : Sol, Si, Ré</div>
-    <div>La majeur (A) : La, Do#, Mi</div>
-    <div>Si majeur (B) : Si, Ré#, Fa#</div>
-    <h1>Accords Mineurs</h1>
-    <div>Do mineur (Cm) : Do, Mib, Sol</div>
-    <div>Ré mineur (Dm) : Ré, Fa, La</div>
-    <div>Mi mineur (Em) : Mi, Sol, Si</div>
-    <div>Fa mineur (Fm) : Fa, Lab, Do</div>
-    <div>Sol mineur (Gm) : Sol, Sib, Ré</div>
-    <div>La mineur (Am) : La, Do, Mi</div>
-    <div>Si mineur (Bm) : Si, Ré, Fa#</div>
-  </div>
+
+    <div style="width: 400px">
+      <h1>Accords Majeurs</h1>
+      <div>Do majeur (C) : Do, Mi, Sol</div>
+      <div>Ré majeur (D) : Ré, Fa#, La</div>
+      <div>Mi majeur (E) : Mi, Sol#, Si</div>
+      <div>Fa majeur (F) : Fa, La, Do</div>
+      <div>Sol majeur (G) : Sol, Si, Ré</div>
+      <div>La majeur (A) : La, Do#, Mi</div>
+      <div>Si majeur (B) : Si, Ré#, Fa#</div>
+      <h1>Accords Mineurs</h1>
+      <div>Do mineur (Cm) : Do, Mib, Sol</div>
+      <div>Ré mineur (Dm) : Ré, Fa, La</div>
+      <div>Mi mineur (Em) : Mi, Sol, Si</div>
+      <div>Fa mineur (Fm) : Fa, Lab, Do</div>
+      <div>Sol mineur (Gm) : Sol, Sib, Ré</div>
+      <div>La mineur (Am) : La, Do, Mi</div>
+      <div>Si mineur (Bm) : Si, Ré, Fa#</div>
+    </div>
   </main>
 </template>
 
---
+++ tablature_accordeon/src/components/AccordLetters.vue
@@ -0,0 +1,32 @@
+<script setup>
+import AccordItem from "./AccordItem.vue";
+</script>
+
+<template>
+  <div class="container">
+      <AccordItem @click="$emit('select-accord', ['Do', 'Mi', 'Sol'])">C</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Do', 'Mib', 'Sol'])">Cm</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Ré', 'Fa#', 'La'])">D</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Ré', 'Fa', 'La'])">Dm</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Mi', 'Sol#', 'Si'])">E</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Mi', 'Sol', 'Si'])">Em</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Fa', 'La', 'Do'])">F</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Fa', 'Sol#', 'Do'])">Fm</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Sol', 'Si', 'Ré'])">G</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Sol', 'Sib', 'Ré'])">Gm</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['La', 'Do#', 'Mi'])">A</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['La', 'Do', 'Mi'])">Am</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Si', 'Ré#', 'Fa#'])">B</AccordItem>
+      <AccordItem @click="$emit('select-accord', ['Si', 'Ré', 'Fa#'])">Bm</AccordItem>
+  </div>
+
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1 1 50%; /* Chaque élément occupe 50% de la largeur du conteneur */
+  padding: 10px; /* Optionnel : pour ajouter de l'espace autour des éléments */
+}
+</style>
